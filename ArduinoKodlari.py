778aa8d475c0f34f596ab8235ed5de1f43fac01b
Abdullah Balcan <90114068+TKN-YZM@users.noreply.github.com>
2023-12-17 15:40:48 +0300

    Update ArduinoKodlari.py

--- ArduinoKodlari.py
+++ ArduinoKodlari.py
@@ -7,31 +7,31 @@ class RobotikIslem():
     
     def __init__(self):
         
-        self.port = 'COM4'
+        self.port = 'COM4'  #İletişim kurulacağı port
         self.board = Arduino(port)
         
     def sensorDeger(self):
         dht_pin = 2 
         dht_sensor = Adafruit_DHT.DHT11
-        nem_deger, sicaklik_deger = Adafruit_DHT.read_retry(dht_sensor, dht_pin)
+        nem_deger, sicaklik_deger = Adafruit_DHT.read_retry(dht_sensor, dht_pin)  
         return [sicaklik_deger,nem_deger]
 
     def klimaIslemi(self,durum):
-        self.board.get_pin("d:7:o") #klima bağlı pin
+        self.board.get_pin("d:7:o") #klima (röle) bağlı pin
         if(durum):
-             board.digital[7].write(1)
+             board.digital[7].write(1) #röle aktif edilir
              board.exit()
         else:
-            board.digital[pin_numarasi].write(0)
+            board.digital[pin_numarasi].write(0) #röle kapatılır
             board.exit()
              
     
     def otomatikKlima(self):
         
-        data=self.sensorDeger()
-        tahmin=ML.TahminAlgorit(data[0], data[1])
+        data=self.sensorDeger()   #sensörden değer alma 
+        tahmin=ML.TahminAlgorit(data[0], data[1])  #tahmin algoritmasına sıcaklık ve nem değerlerini gönderme
         if(tahmin==1):
-            self.klimaIslemi(1)
+            self.klimaIslemi(1)  
             return 1
         else:
             self.klimaIslemi(0)
