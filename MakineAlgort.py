5503716d2cd80ea9dcb4c6833c855a593cc4629c
Abdullah Balcan <90114068+TKN-YZM@users.noreply.github.com>
2024-01-30 17:27:48 +0300

    Update MakineAlgort.py

--- MakineAlgort.py
+++ MakineAlgort.py
@@ -1,24 +1,23 @@
 import pandas as pd
-import numpy as np
-import sqlite3
 import VeriTabani as DB
-from sklearn.metrics import confusion_matrix #Performans ölçümü
-from sklearn.linear_model import LogisticRegression  #ML Algoritması
-from sklearn.model_selection import GridSearchCV  #En optimum parametre 
-from sklearn.model_selection import train_test_split 
+from sklearn.metrics import confusion_matrix
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV 
+from sklearn.model_selection import train_test_split
 
 
 class MakineAlgorit():
     
     def __init__(self):
-        
-        self.db=DB.DataBase()
-        self.data=self.db.veriTabaniGetir()  #Tüm verilerin getirilmesi
-        sicaklik_nem=self.data[["Sıcaklık","Nem"]] 
-        klima_deger=self.data[["KlimaAcik"]]
-        print(self.data.corr()) #Veriler arası korelasyon gözetme
-        self.x_Train,self.x_Test,self.y_Train,self.y_Test=train_test_split(sicaklik_nem,klima_deger,random_state=2,test_size=0.2)
-    
+
+        self.db = DB.DataBase()
+        self.data = self.db.veriTabaniGetir()
+        sicaklik_nem = self.data[["Sıcaklık", "Nem"]]
+        klima_deger = self.data[["KlimaAcik"]]
+        # print(self.data.corr())
+        self.x_Train, self.x_Test, self.y_Train, self.y_Test = train_test_split(sicaklik_nem, klima_deger,
+                                                                                random_state=2, test_size=0.2)
+
     #Algoritamanın öğrenim/tahmin yüzdeliği
     def __matrixOran(self,true_table,predict_table):
         
@@ -34,13 +33,12 @@ class MakineAlgorit():
             # False Negative (FN)
             fn = cf[1, 0]
             istatistic=(tp+tn)/(tp+fp+tn+fn)*100
-            print("İstatistik: ",istatistic)
+            print("Algoritma Başarı İstatistik: ",istatistic)
             return (tp+tn)/(tp+fp+tn+fn)*100
     
          else:
             print("Hata: Geçerli bir tablo verisi bekleniyor.")
 
-    
     #Parametre Optimizasyonunun yapılması (GridSearch ile)
     def __parametreOptim(self,clsfr,p: list):
         
@@ -61,19 +59,14 @@ class MakineAlgorit():
         
     #LogisticRegression kullanımı
     def TahminAlgorit(self,sicaklik_deg:int,nem_deger:int):
-        
-        lg=LogisticRegression(solver='newton-cg')
-        lg.fit(self.x_Train,self.y_Train)
-        predict=lg.predict(self.x_Test)
-        
-        self.__matrixOran(self.y_Test, predict)
-        
+        lg=LogisticRegression()
+        lg.fit(self.x_Train.values,self.y_Train.values.ravel())
+        predicta=lg.predict(self.x_Test.values)
+        self.__matrixOran(self.y_Test, predicta)
         '''
             print("LG OPTIMUM")
             parametreOptim(lg, [{'solver':['newton-cg', 'lbfgs', 'liblinear', 'sag']}])
         '''
-        
-        return lg.predict([[sicaklik_deg,nem_deger]])
-       
+        return lg.predict([[float(sicaklik_deg),float(nem_deger)]])
 
 
