09f78aa2913ca2132384e2eb8f7b4fa3a9796786
Abdullah Balcan <90114068+TKN-YZM@users.noreply.github.com>
2024-01-30 17:28:06 +0300

    Update VeriTabani.py

--- VeriTabani.py
+++ VeriTabani.py
@@ -4,25 +4,22 @@ import pandas as pd
 class DataBase:
     
     def __init__(self):
-        #veri tabanı python bağlantıları
         self.conn=sqlite3.connect("KlimaVerileri.db")
         self.cursor=self.conn.cursor()
         self.__tabloOlustur()
-   
-        
+
     def __tabloOlustur(self):
-        #veri tabanı yoksa oluşturma
+        
         self.cursor.execute("Create Table if not exists KlimaTablo (Sıcaklık Real,Nem Real,KlimaAcik Int)")
         self.conn.commit()
-  
-        
+
     def veriEkle(self,sicaklik: float,nem: float,klima_acik: int):
         
         data=self.veriTabaniGetir() #tüm veriler
         flag=False
         print(data)
         
-        for indeks,satir in data.iterrows(): #tablo verilerini parçala
+        for indeks,satir in data.iterrows(): #tablo verilerini aparçala
             sicaklik_tablo=satir["Sıcaklık"]
             nem_tablo=satir["Nem"]
             durum_tablo=satir["KlimaAcik"]
@@ -30,36 +27,25 @@ class DataBase:
             #aynı veri varsa overfitting riski sebebiyle ekleme yapmıyoruz
             if(sicaklik_tablo==sicaklik and nem_tablo==nem and durum_tablo==klima_acik): 
                 flag=True
-         
-                
+
         if(flag==False):
-             print("Data eklendi")
              x="Insert into KlimaTablo Values (?,?,?)"
              self.cursor.execute(x,(sicaklik,nem,klima_acik))
-             self.conn.commit() 
-            
-        
+             self.conn.commit()
+             print("Data eklendi")
+
+
     def veriGoster(self):
-        #tüm verilerin getirilmesi  / proje aşamasında verilerin gözetilmesi için oluşturulmuş hazır fonksiyon
         self.cursor.execute("Select * from KlimaTablo")
         liste=self.cursor.fetchall()
         print("Sıcaklık |Nem |Klima Durumu")
         for x in liste:
             print(x )
         self.conn.commit()
-       
-        
+
     def veriTabaniGetir(self):
         sql_query = "SELECT * FROM KlimaTablo;"
-        conn=sqlite3.connect("KlimaVerileri.db")
-        
-        data = pd.read_sql(sql_query, conn)
-        
+        data = pd.read_sql(sql_query, self.conn)
         return data
-        
-    
-
-     
 
 
-    
